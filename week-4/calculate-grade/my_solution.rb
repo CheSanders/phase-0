67b9ca245806f18b197568ccdbec37767c9a8cec
Ché Sanders <sandersche@yahoo.com>
2016-01-25 01:19:04 -0800

    update

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,21 @@
+def get_grade(num)
+  if num >= 90
+    return "A"
+  end
+
+  if num >= 80 && num <= 89
+    return "B"
+  end
+
+  if num >= 70 && num <= 79
+    return "C"
+  end
+  
+  if num >= 60 && num <= 69
+    return "D"
+  end
+  
+  if num <= 59
+    return "F"
+  end
+end
