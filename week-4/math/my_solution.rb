276ee54e259157d62869def8fe08d63efef73f65
Ché Sanders <sandersche@yahoo.com>
2016-01-24 19:19:30 -0800

    math done

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,22 @@
+# Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(x,y)
+  x + y
+end
+
+def subtract(x,y)
+  x - y
+end
+
+def multiply(x,y)
+  x * y
+end
+
+def divide(x,y)
+ x.to_f / y.to_f
+end
